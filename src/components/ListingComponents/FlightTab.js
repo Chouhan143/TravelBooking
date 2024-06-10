926e6b9ca3af3e669177e04f67e40419077b2c18
pallavi patel <pallavipatel782@gmail.com>
2024-06-10 10:54:34 +0530

    changes

--- src/components/ListingComponents/FlightTab.js
+++ src/components/ListingComponents/FlightTab.js
@@ -83,8 +83,8 @@ const FlightTab = props => {
           Origin: fromCity,
           Destination: toCity,
           FlightCabinClass: state.FloorNumber,
-          PreferredDepartureTime: '2024-06-07T00:00:00',
-          PreferredArrivalTime: '2024-06-07T00:00:00',
+          PreferredDepartureTime: '2024-10-07T00:00:00',
+          PreferredArrivalTime: '2024-10-07T00:00:00',
         },
       ],
     };
